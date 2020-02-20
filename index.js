e0754128efca6d6bd0dfdfb9304fe54c63f831ad
Shea Mentges <shea.mentges@gmail.com>
2020-02-19 23:03:53 -0800

    feat: added dispatch method

--- index.js
+++ index.js
@@ -19,8 +19,14 @@ function createStore() {
         }
     }
 
+    const dispatch = (action) => {
+        state = todos(state, action)
+        listeners.forEach((listener) => listener())
+    }
+
     return {
         getState,
-        subscribe
+        subscribe,
+        dispatch
     }
 }
